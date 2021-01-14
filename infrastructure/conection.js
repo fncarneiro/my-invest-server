64fffaff9941c73ec94a3cd92eee2a41265ee117
Fernando Nascimento Carneiro <fernando.carneiro@dataprev.gov.br>
2021-01-14 19:57:01 -0300

    new db configs

--- infrastructure/conection.js
+++ infrastructure/conection.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql');
-const pool = mysql.createPool({
-    connectionLimit : process.env.MYSQL_LIMIT,
-    host: process.env.MYSQL_HOST, 
-    port: process.env.MYSQL_PORT,   
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-});
+const pool = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
+
+// const pool = mysql.createPool({
+//     connectionLimit : process.env.MYSQL_LIMIT,
+//     host: process.env.MYSQL_HOST, 
+//     port: process.env.MYSQL_PORT,   
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.MYSQL_DATABASE
+// });
 
 pool.on('release', () => console.log('MySql pool => connection returned')); 
 
--- server.js
+++ server.js
@@ -5,33 +5,24 @@ const tables = require('./infrastructure/tables');
 
 const port = process.env.PORT || 3000;
 
-// conection.getConnection((error, conn) => {
-//     if (error) {
-//         console.log(`Error connecting DB my-invest on port ${process.env.MYSQL_PORT}.`) 
-//     } else {
-//         const app = customExpress();  
+conection.getConnection((error, conn) => {
+    if (error) {
+        console.log(`Error connecting DB my-invest on port ${process.env.MYSQL_PORT}.`) 
+    } else {
+        const app = customExpress();  
         
-//         tables.init(conn);
-//         conn.release;      
+        tables.init(conn);
+        conn.release;      
 
-//         app.listen(port, (error) => {
-//             if (error) {
-//                 return console.log(`Anything goes wrong on starting server - ${error}`)
-//             }
-//             else {                
-//                 console.log(`Server running on port ${port}, Host ${process.env.HOST} ...`)
-//             }
-//         });
-//     }
-const app = customExpress(); 
-app.listen(port, (error) => {
-                if (error) {
-                    return console.log(`Anything goes wrong on starting server - ${error}`)
-                }
-                else {                
-                    console.log(`Server running on port ${port}, Host ${process.env.HOST} ...`)
-                }
-            });
-
+        app.listen(port, (error) => {
+            if (error) {
+                return console.log(`Anything goes wrong on starting server - ${error}`)
+            }
+            else {                
+                console.log(`Server running on port ${port}, Host ${process.env.HOST} ...`)
+            }
+        });
+    }
+}) 
 
 
